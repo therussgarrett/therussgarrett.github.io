264fd008ba5756d6f714a9d9d939b054c016147c
Russell-Savage <opticon.russ@gmail.com>
2026-05-23 05:56:12 -0600

    update email setting on form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact({setActiveSection}){
                 <div className="justify-items-center not-prose overflow-auto rounded-lg outline outline-white/5 bg-zinc-800/50 p-8">
                   <div className="w-auto flex flex-col lg:w-3/6">
               <p className="text-xl">Send me a message via the form below or LinkedIn.</p>
-              <form action="https://formsubmit.co/russ@fixwithruss.com" method="POST">
+              <form action="https://formsubmit.co/therussgarrett@gmail.com" method="POST">
                 <input className="w-full shadow-md w-auto p-2 my-1 border-1 border-orange-500 rounded-md" type="text" name="name" placeholder="Your Name" required />
                 <input className="w-full shadow-md w-auto p-2 my-1 border-1 border-orange-500 rounded-md" type="email" name="email" placeholder="Your Email" required />
                 <textarea className="w-full shadow-md w-auto p-2 my-1 border-1 border-orange-500 rounded-md" name="message" rows="5" placeholder="Your Message" required></textarea>
